33bd0143c52fc325191e669d7ede4e26a9c8d38b
uri datz <uridatz@gmail.com>
2017-01-07 16:59:25 +0200

    added blank pages for:
     1) manage friends
     2) video chat hub
     3) remote control
    
     *all the pages are accesable from the profile activity

--- app/src/main/java/com/uri/gal/firetest/ProfileActivity.java
+++ app/src/main/java/com/uri/gal/firetest/ProfileActivity.java
@@ -14,7 +14,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener
 {
     private FirebaseAuth firebaseAuth;
-    private Button buttonLogout, buttonEditProfile;
+    private Button buttonLogout, buttonEditProfile, buttonRemoteControl, buttonVideoChat, buttonManageFriends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -41,13 +41,22 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         }
 
         TextView textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
-
+        
         textViewUserEmail.setText("Welcome " + user.getEmail());
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
         buttonLogout.setOnClickListener(this);
 
         buttonEditProfile =  (Button) findViewById(R.id.buttonEditProfile);
         buttonEditProfile.setOnClickListener(this);
+
+        buttonRemoteControl = (Button) findViewById(R.id.buttonRemoteControl);
+        buttonRemoteControl.setOnClickListener(this);
+
+        buttonVideoChat = (Button) findViewById(R.id.buttonVideoChat);
+        buttonVideoChat.setOnClickListener(this);
+
+        buttonManageFriends  = (Button) findViewById(R.id.buttonManageFriends);
+        buttonManageFriends.setOnClickListener(this);
     }
 
         @Override
@@ -64,6 +73,20 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 finish();
                 startActivity(new Intent(this, EditProfileActivity.class));
             }
-
+            else if(view == buttonRemoteControl)
+            {
+                finish();
+                startActivity(new Intent(this, RemoteControlActivity.class));
+            }
+            else if(view == buttonVideoChat)
+            {
+                finish();
+                startActivity(new Intent(this, VideoChatHubActivity.class));
+            }
+            else if(view == buttonManageFriends)
+            {
+                finish();
+                startActivity(new Intent(this, ManageFriendsActivity.class));
+            }
         }
 }
